15bc0632926710ede31834d8619146e28eb1ae31
Shiva Raj Badu <shivabadu5@gmail.com>
2023-06-22 21:41:41 +0545

    seller changes

--- public/js/seller_main.js
+++ public/js/seller_main.js
@@ -3,6 +3,36 @@
  *
  * ------------------------------------------------------------------- */
 
+// FAQ toggler
+let toggles = document.getElementsByClassName("toggle");
+let contentDiv = document.getElementsByClassName("content");
+let icons = document.getElementsByClassName("icon");
+
+for (let i = 0; i < toggles.length; i++) {
+  toggles[i].addEventListener("click", () => {
+    if (parseInt(contentDiv[i].style.height) != contentDiv[i].scrollHeight) {
+      contentDiv[i].style.height = contentDiv[i].scrollHeight + "px";
+      toggles[i].style.color = "#0084e9";
+      icons[i].classList.remove("fa-plus");
+      icons[i].classList.add("fa-minus");
+    } else {
+      contentDiv[i].style.height = "0px";
+      toggles[i].style.color = "#111130";
+      icons[i].classList.remove("fa-minus");
+      icons[i].classList.add("fa-plus");
+    }
+
+    for (let j = 0; j < contentDiv.length; j++) {
+      if (j !== i) {
+        contentDiv[j].style.height = 0;
+        toggles[j].style.color = "#111130";
+        icons[j].classList.remove("fa-minus");
+        icons[j].classList.add("fa-plus");
+      }
+    }
+  });
+}
+//]]>
 (function ($) {
   "use strict";
 
@@ -105,28 +135,28 @@
 
   /* Smooth Scrolling
    * ------------------------------------------------------ */
-  var ssSmoothScroll = function () {
-    $(".smoothscroll").on("click", function (e) {
-      var target = this.hash,
-        $target = $(target);
+  // var ssSmoothScroll = function () {
+  //   $(".smoothscroll").on("click", function (e) {
+  //     var target = this.hash,
+  //       $target = $(target);
 
-      e.preventDefault();
-      e.stopPropagation();
+  //     e.preventDefault();
+  //     e.stopPropagation();
 
-      $("html, body")
-        .stop()
-        .animate(
-          {
-            scrollTop: $target.offset().top,
-          },
-          cfg.scrollDuration,
-          "swing",
-          function () {
-            window.location.hash = target;
-          }
-        );
-    });
-  };
+  //     $("html, body")
+  //       .stop()
+  //       .animate(
+  //         {
+  //           scrollTop: $target.offset().top,
+  //         },
+  //         cfg.scrollDuration,
+  //         "swing",
+  //         function () {
+  //           window.location.hash = target;
+  //         }
+  //       );
+  //   });
+  // };
 
   /* Placeholder Plugin Settings
    * ------------------------------------------------------ */
@@ -212,7 +242,7 @@
     ssFitVids();
     ssOwlCarousel();
     ssWaypoints();
-    ssSmoothScroll();
+    // ssSmoothScroll();
     ssPlaceholder();
     ssAlertBoxes();
     ssAOS();
